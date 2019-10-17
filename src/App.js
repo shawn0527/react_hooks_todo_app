2bb9c36e0bc35389cfb2f916a18b766c481d3490
shawn0527 <shawnren527@gmail.com>
2019-10-17 11:05:15 -0500

    add to do form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { useState } from 'react';
 import './App.css'
 
 function Todo({todo, index}) {
-return(
-  <div className="todo">
-    {`${index+1} `+todo.text}
-  </div>
-)
+return <div className="todo">{`${index+1} `+todo.text}</div>
+}
+
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('')
+
+  const handleSubmit =  e => {
+    e.preventDefalut()
+    if(!value) return;
+    addTodo(value);
+    setValue('')
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} placeholder="Add todo..." onChange={e => setValue(e.target.value)} />
+    </form>
+  )
 }
 
 function App() {
@@ -25,12 +38,18 @@ function App() {
     }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, text]
+    setTodo(newTodos)
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   )
